eb0614f9fc4638d75b3dd6c3d5f50144f2b614ce
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2025-02-24 22:34:02 +0200

    Chart page 50% done

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('chart', function () {
+    return Inertia::render('Chart');
+})->middleware(['auth', 'verified'])->name('chart');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
